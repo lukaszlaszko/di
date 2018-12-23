14d87a5fbbd18216b5c6b2030df0f9a015b04c15
Lukasz Laszko <lukaszlaszko@gmail.com>
2018-12-23 12:24:15 +0800

    conan recipe added.

---
+++ conanfile.py
@@ -0,0 +1,37 @@
+from conans import ConanFile, CMake
+
+
+class DiConan(ConanFile):
+    name = "di"
+    version = "1.0"
+    license = "Apache-2.0"
+    author = "Lukasz Laszko lukaszlaszko@gmail.com"
+    url = "https://github.com/lukaszlaszko/di"
+    description = "<Description of Di here>"
+    topics = ("dependency injection", "di", "c++", "cpp", "", "cxx")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+    exports_sources = "src/*"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="src")
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s'
+        #          % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="include", src="src")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["di"]
